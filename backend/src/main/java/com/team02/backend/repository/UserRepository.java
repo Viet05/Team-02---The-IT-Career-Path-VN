c125b40a879c20bd16a518ab442f8a40cdfd297e
Ngọc Việt <VietDangg2005@gmail.com>
2026-01-11 22:02:06 +0700

    update

--- backend/src/main/java/com/team02/backend/repository/UserRepository.java
+++ backend/src/main/java/com/team02/backend/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package com.team02.backend.repository;
 
 import com.team02.backend.entity.Users;
+import com.team02.backend.enums.UserRole;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +13,5 @@ public interface UserRepository extends JpaRepository<Users, Long> {
   boolean existsByUsername(String username);
   boolean existsByEmail(String email);
   Users findByVerificationToken(String token);
+  List<Users> findByRole(UserRole role);
 }
